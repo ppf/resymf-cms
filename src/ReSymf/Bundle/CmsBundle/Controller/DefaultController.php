f64c8b4c2d7d3a83ab984457df3dd79de7354b33
Piotr Francuz <francuz256@gmail.com>
2014-01-24 12:37:43 +0100

    added missing DefaultController

---
+++ src/ReSymf/Bundle/CmsBundle/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace ReSymf\Bundle\CmsBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+/**
+ * Class DefaultController
+ * @package ReSymf\Bundle\CmsBundle\Controller
+ *
+ * @author Piotr Francuz <francuz256@gmail.com>
+ */
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+
+    }
+}
---
+++ src/ReSymf/Bundle/CmsBundle/Services/ObjectHistory.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ppf
+ * Date: 02.01.14
+ * Time: 23:26
+ */
+
+namespace ReSymf\Bundle\CmsBundle\Services;
+
+/**
+ * Class ObjectHistory
+ * @package ReSymf\Bundle\CmsBundle\Services
+ *
+ * @author Piotr Francuz <francuz256@gmail.com>
+ */
+class ObjectHistory {
+
+    function __construct()
+    {
+    }
+
+    public function createHistoryTable()
+    {
+
+    }
+
+    public function getLastFromHistory($offset = 0)
+    {
+
+    }
+
+    public function clearHistory()
+    {
+
+    }
+
+    public function  saveToHistory()
+    {
+
+    }
+}
